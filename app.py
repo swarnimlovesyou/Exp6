1347e935ec4fa1f87f1220c60951806705cbe75a
Swarnim Jambhrunkar <swarnimjambhrunkar@gmail.com>
2026-03-17 11:26:32 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,8 @@ def home():
     <h2>hobbies: Reading, Swimming, Coding</h2>
     <h2>AppID: 2409789</h2>
     """
-
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
